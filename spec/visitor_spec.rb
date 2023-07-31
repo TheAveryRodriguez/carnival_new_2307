993cc8b0ce483530b641d31a601a83632fadfa8a
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-31 10:16:58 -0500

    created the instance test and attributes test for visitor class

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -8,15 +8,19 @@ describe Visitor do
       expect(visitor1).to be_a(Visitor)
     end
   end
+
+  it "has attributes" do
+    visitor1 = Visitor.new("Bruce", 54, "$10")
+
+    expect(visitor1.name).to eq("Bruce")
+
+    expect(visitor1.height).to eq(54)
+
+    expect(visitor1.spending_money).to eq(10)
+
+    expect(visitor1.preferences).to eq([])
+  end
 end
-
-# expect(visitor1.name).to eq("Bruce")
-
-# expect(visitor1.height).to eq(54)
-
-# expect(visitor1.spending_money).to eq(10)
-
-# expect(visitor1.preferences).to eq([])
 
 # visitor1.add_preference(:gentle)
 
