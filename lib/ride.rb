8d1201eb9ee9135aa32e4c35b4b71f6143d99330
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-31 12:54:32 -0500

    fix the board_rider method by adding a private can_ride method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,5 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
+  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue, :rider_log
 
   def initialize(ride_info)
     @name = ride_info[:name]
@@ -7,14 +7,24 @@ class Ride
     @admission_fee = ride_info[:admission_fee]
     @excitement = ride_info[:excitement]
     @total_revenue = 0
+    @rider_log = {}
   end
 
   # def total_revenue
   # end
 
-  def board_rider(rider)
+  def board_rider(visitor)
+    if can_ride?(visitor)
+      @rider_log[visitor] ||= 0
+      @rider_log[visitor] += 1
+    else
+      raise "heck"
+    end
   end
 
-  def rider_log
+  def can_ride?(visitor)
+    visitor.tall_enough?(@min_height) &&
+      visitor.preferences.include?(@excitement) &&
+      visitor.spending_money >= @admission_fee
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -23,18 +23,19 @@ describe Ride do
 
   describe "#board_rider" do
     it "adds a rider to a ride if they qualify" do
+      ride1 = Ride.new({name: "Carousel", min_height: 24, admission_fee: 1, excitement: :gentle})
       visitor1 = Visitor.new("Bruce", 54, "$10")
 
       visitor1.add_preference(:gentle)
-
       ride1.board_rider(visitor1)
 
-      expect(ride1.rider_log).to eq(visitor1)
+      expect(ride1.rider_log).to eq({visitor1 => 1})
     end
   end
 
   describe "#rider_log" do
     it "tracks who has ridden a ride and how many times" do
+      ride1 = Ride.new({name: "Carousel", min_height: 24, admission_fee: 1, excitement: :gentle})
       visitor1 = Visitor.new("Bruce", 54, "$10")
       visitor2 = Visitor.new("Tucker", 36, "$5")
 
@@ -45,52 +46,39 @@ describe Ride do
       ride1.board_rider(visitor2)
       ride1.board_rider(visitor1)
 
-      expect(ride1.rider_log).to eq(visitor1, visitor2)
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+    end
+  end
+
+  describe "#total_revenue" do
+    it "calculates a rides total revenue earned" do
+      ride1 = Ride.new({name: "Carousel", min_height: 24, admission_fee: 1, excitement: :gentle})
+      visitor1 = Visitor.new("Bruce", 54, "$10")
+      visitor2 = Visitor.new("Tucker", 36, "$5")
+
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor2.spending_money).to eq(4)
+
+      expect(ride1.total_revenue).to eq(3)
+
+      visitor3 = Visitor.new("Penny", 64, "$15")
+      ride2 = Ride.new({name: "Ferris Wheel", min_height: 36, admission_fee: 5, excitement: :gentle})
+      ride3 = Ride.new({name: "Roller Coaster", min_height: 54, admission_fee: 2, excitement: :thrilling})
+
+      visitor2.add_preference(:thrilling)
+      visitor3.add_preference(:thrilling)
+
+      ride3.board_rider(visitor1)
+      ride3.board_rider(visitor2)
+      ride3.board_rider(visitor3)
+
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor2.spending_money).to eq(4)
+      expect(visitor3.spending_money).to eq(13)
+
+      expect(ride3.rider_log).to eq({visitor3 => 1})
+
+      expect(ride3.total_revenue).to eq(2)
     end
   end
 end
-
-# expect(visitor1.spending_money
-# #=> 8
-
-# expect(visitor2.spending_money
-# #=> 4
-
-# expect(ride1.total_revenue
-# #=> 3
-
-# visitor3 = Visitor.new('Penny', 64, '$15')
-# #=> #<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[], @spending_money=15>
-
-# ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-# #=> #<Ride:0x0000000159a0cd00 @admission_fee=5, @excitement=:gentle, @min_height=36, @name="Ferris Wheel", @rider_log={}>
-
-# ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
-# #=> #<Ride:0x0000000159ae7a68 @admission_fee=2, @excitement=:thrilling, @min_height=54, @name="Roller Coaster", @rider_log={}>
-
-# visitor2.add_preference(:thrilling)
-# #=> [:gentle, :thrilling]
-
-# visitor3.add_preference(:thrilling)
-# #=> [:thrilling]
-
-# ride3.board_rider(visitor1)
-
-# ride3.board_rider(visitor2)
-
-# ride3.board_rider(visitor3)
-
-# expect(visitor1.spending_money
-# #=> 8
-
-# expect(visitor2.spending_money
-# #=> 4
-
-# expect(visitor3.spending_money
-# #=> 13
-
-# expect(ride3.rider_log
-# #=> {#<Visitor:0x0000000159a852a0 @height=64, @name="Penny", @preferences=[:thrilling], @spending_money=13> => 1}
-
-# expect(ride3.total_revenue
-# #=> 2
